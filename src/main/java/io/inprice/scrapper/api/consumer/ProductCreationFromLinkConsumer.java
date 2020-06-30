91a3f0b3fb8f2df9b3f3bc7e3fc9168c541336af
mdpinar <mdumlupinar@gmail.com>
2020-06-30 20:54:03 +0300

    manuel ack approach in rabbitmq consumers removed

--- src/main/java/io/inprice/scrapper/api/consumer/ProductCreationFromLinkConsumer.java
+++ src/main/java/io/inprice/scrapper/api/consumer/ProductCreationFromLinkConsumer.java
@@ -38,15 +38,12 @@ public class ProductCreationFromLinkConsumer {
       @Override
       public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) {
         ThreadPools.PRODUCT_CREATION_POOL.submit(() -> {
-          boolean created = false;
           ServiceResponse res = Responses.NotFound.COMPETITOR;
           try {
             Competitor link = JsonConverter.fromJson(new String(body), Competitor.class);
             if (link != null) {
               res = productRepository.createFromLink(link);
-              if (res.isOK()) {
-                created = true;
-              } else {
+              if (! res.isOK()) {
                 log.error("DB problem while activating a competitor!");
               }
             } else {
@@ -54,24 +51,14 @@ public class ProductCreationFromLinkConsumer {
             }
 
           } catch (Exception e) {
-            log.error("Failed to submit tasks into ThreadPool", e);
-          }
-
-          try {
-            if (created) {
-              channel.basicAck(envelope.getDeliveryTag(), false);
-            } else {
-              channel.basicNack(envelope.getDeliveryTag(), false, false);
-            }
-          } catch (IOException e1) {
-            log.error("Failed to send a message to dlq", e1);
+            log.error("Failed to create product from link", e);
           }
         });
       }
     };
 
     try {
-      channel.basicConsume(SysProps.MQ_PRODUCT_CREATION_QUEUE(), false, consumer);
+      channel.basicConsume(SysProps.MQ_PRODUCT_CREATION_QUEUE(), true, consumer);
     } catch (IOException e) {
       log.error("Failed to set a queue for creating products from link.", e);
     }
